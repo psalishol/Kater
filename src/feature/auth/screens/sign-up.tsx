2a700e4777eb426e52c07dbf5c494884b3ea06ac
psalishol <psalishol80@gmail.com>
2023-11-21 16:04:59 +0100

    update: refactor and split component to module

--- src/feature/auth/screens/sign-up.tsx
+++ src/feature/auth/screens/sign-up.tsx
@@ -1,28 +1,38 @@
-import {useNavigation} from '@react-navigation/native';
-import {AntDesign, Box, Pressable, Text} from '../../../components/atom';
-import React, {useCallback} from 'react';
+import {Box, Text} from '../../../components/atom';
+import React from 'react';
 import {GradientBackground} from '../../../components/organism';
+import {screenHeight} from '../../../constant';
+import {fonts} from '../../../themes/fonts';
+import {
+  SignUserUpButton,
+  SignupBackPressButton,
+  SignupButton,
+  SignupEmailInput,
+  SignupNameInput,
+  SignupPasswordInput,
+} from '../components/molecules';
 
 const SignUp: React.FunctionComponent = () => {
   return (
     <GradientBackground>
-      <SignupBackPress />
+      <SignupBackPressButton />
+
+      <Box style={{marginTop: screenHeight * 0.13}} />
+      <Text fontSize={16} mx="md" fontFamily={fonts.RobotoMedium}>
+        Lets get you started real quick!!
+      </Text>
+      <Text mx="md" mt="xs" fontSize={14} opacity={0.5}>
+        Kindly fill in the neccessary credential. Please endeavour to put in the
+        right detail.
+      </Text>
+
+      <SignupNameInput />
+      <SignupEmailInput />
+      <SignupPasswordInput />
+      <SignupButton />
+      <SignUserUpButton />
     </GradientBackground>
   );
 };
 
 export default SignUp;
-
-const SignupBackPress = () => {
-  const navigation = useNavigation();
-
-  const handlePress = useCallback(() => {
-    navigation.goBack();
-  }, [navigation]);
-
-  return (
-    <Pressable mt="md" alignSelf="baseline" p="sm" onPress={handlePress}>
-      <AntDesign name="arrowleft" color={'$black'} />
-    </Pressable>
-  );
-};
