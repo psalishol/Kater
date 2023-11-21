e8919f7467a11806cc3b7fdd27c5c9caffc4125d
psalishol <psalishol80@gmail.com>
2023-11-21 10:41:38 -0800

    update container layout

--- src/feature/home/component/molecules/location-changer-button.tsx
+++ src/feature/home/component/molecules/location-changer-button.tsx
@@ -1,5 +1,5 @@
 import {useAtom, useAtomValue, useSetAtom} from 'jotai';
-import {Entypo, Text, Touchable} from '../../../../components/atom';
+import {AntDesign, Entypo, Text, Touchable} from '../../../../components/atom';
 import {fonts} from '../../../../themes/fonts';
 import {useCallback, useEffect} from 'react';
 import {
@@ -8,6 +8,7 @@ import {
   openChangeLocation,
 } from '../../../location';
 import {getLocation} from '../../../location/util';
+import {size} from '../../../../helper';
 
 const LocationChangerButton: React.FunctionComponent = () => {
   const setOpenChangeLocationSheet = useSetAtom(openChangeLocation);
@@ -35,12 +36,14 @@ const LocationChangerButton: React.FunctionComponent = () => {
 
   return (
     <Touchable
+      position="absolute"
       px="xs"
-      mx="sm"
+      mx="xs"
       py="xxs"
       borderRadius="xs"
+      top={size(15)}
       onPress={handlePress}
-      style={{backgroundColor: 'rgba(0,0,0,0.1)'}}
+      style={{backgroundColor: 'rgba(0,0,0,0.03)'}}
       alignSelf="baseline"
       mt="md"
       flexDirection="row"
@@ -49,6 +52,7 @@ const LocationChangerButton: React.FunctionComponent = () => {
       <Text ml="xs" fontFamily={fonts.InterMedium} color="$black">
         {currentLocation.city}, {currentLocation.state}
       </Text>
+      <AntDesign name="down" size={13} marginLeft="xs" color={'$black'} />
     </Touchable>
   );
 };
