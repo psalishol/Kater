8c580f191d4bc79aff201035266531d50deb715c
psalishol <psalishol80@gmail.com>
2023-11-21 06:32:10 -0800

    add atom: location filter atom and sheet open atom

--- src/feature/location/state/index.ts
+++ src/feature/location/state/index.ts
@@ -23,7 +23,12 @@ export interface Location {
 export const currentLocationAtom = atom<Location | undefined>(undefined);
 
 //holds the current country city and state
-export const countryCityStateAtom = atom<string[] | undefined>(undefined);
+export const countryCityStateAtom = atom<
+  {city: string; state: string}[] | undefined
+>(undefined);
 
 // state to open change location sheet
 export const openChangeLocation = atom<boolean>(false);
+
+// Store filter query string for location in modal input.
+export const locationFilterQueryAtom = atom<string>('');
