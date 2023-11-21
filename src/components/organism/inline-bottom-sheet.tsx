dd25b9991517b47655b5995317bddd6f1d61fbc7
psalishol <psalishol80@gmail.com>
2023-11-21 06:26:56 -0800

    fix: overlay press fixed.

--- src/components/organism/inline-bottom-sheet.tsx
+++ src/components/organism/inline-bottom-sheet.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unstable-nested-components */
 import React, {
   memo,
   useCallback,
@@ -7,135 +6,63 @@ import React, {
   useRef,
   useState,
 } from 'react';
-
+import LiftView from '../../components/organism/liftview';
 import {default as RNBottomSheet} from '@gorhom/bottom-sheet';
-import {ResponsiveColorPropType} from '../../themes/theme';
-import Liftview from './liftview';
-import {AntDesign, Box, Pressable} from '../atom';
-import {size} from '../../helper';
+import {Box} from '../atom';
 
 interface Props {
   show: boolean;
-
   renderView?: () => JSX.Element;
-
   overlayOpacity?: number;
-
+  snapPoints: Array<string>;
   useHandle?: boolean;
-
   onOverlayPressed?: () => void;
-
   closeSheet: () => void;
-
-  zIndex?: number;
-
-  index?: number;
-
-  useSecondOverlay?: boolean;
-
-  onPressSecondOverlay?: () => void;
-
-  immediatelyClose?: boolean;
-
-  overlayColor?: ResponsiveColorPropType;
-
-  closeColor?: ResponsiveColorPropType;
 }
 
 const InlineBottomSheet: React.FunctionComponent<Props> = props => {
-  const {
-    show,
-    renderView,
-    useHandle,
-    overlayOpacity = 0.1,
-    closeSheet,
-    zIndex,
-    useSecondOverlay,
-    onPressSecondOverlay,
-    immediatelyClose,
-    overlayColor,
-
-    closeColor = '$black',
-  } = props;
+  const {show, renderView, snapPoints, useHandle, overlayOpacity, closeSheet} =
+    props;
 
   const [openSheet, setOpenSheet] = useState<boolean>(false);
-
-  const [animated, setAnimated] = useState<boolean>(false);
-  const [close, setClose] = useState<boolean>(false);
 
   const ref = useRef<RNBottomSheet>(null);
 
   useEffect(() => {
     if (show) {
       setOpenSheet(show);
-      setClose(false);
     }
     show && ref.current?.snapToIndex(0);
     !show && ref.current?.close();
   }, [show]);
 
-  const snapsTo = useMemo(() => ['65%'], []);
-  const handleOverlayPressed = useCallback(() => {
-    setClose(true);
-    ref.current?.close();
-  }, []);
+  const snapsTo = useMemo(() => ['75%', '100%'], []);
+
+  const handleOverlayPressed = useCallback(() => ref.current?.close(), []);
 
   const onSheetClose = useCallback(() => {
     setOpenSheet(false);
     closeSheet();
   }, [closeSheet]);
 
-  useEffect(() => {
-    if (immediatelyClose) {
-      onSheetClose();
-    }
-  }, [immediatelyClose, onSheetClose]);
-
-  if (immediatelyClose) {
-    return <></>;
-  }
-
   return (
-    <Liftview
-      useSecondOverlay={true}
-      onPressSecondOverlay={() => {}}
+    <LiftView
       opacity={overlayOpacity}
       lift={openSheet}
       close={show}
-      bg={overlayColor ?? '$overlayColor'}
-      zIndex={zIndex}
+      bg={'$overlayColor'}
       onOverlayPress={handleOverlayPressed}
       renderView={() => {
         return (
-          <>
-            {!close && (
-              <Pressable
-                onPress={handleOverlayPressed}
-                p={'xs'}
-                // opacity={0.5}
-                alignSelf={'center'}
-                position={'absolute'}
-                top="27%">
-                <AntDesign
-                  size={size(30)}
-                  color={closeColor}
-                  name="closecircle"
-                />
-              </Pressable>
-            )}
-            <RNBottomSheet
-              containerStyle={{zIndex}}
-              // backgroundStyle={{zIndex, backgroundColor}}
-              style={{zIndex}}
-              enablePanDownToClose
-              handleComponent={useHandle ? undefined : () => <Box />}
-              ref={ref}
-              onChange={() => setAnimated(true)}
-              onClose={onSheetClose}
-              snapPoints={snapsTo}>
-              {renderView && renderView()}
-            </RNBottomSheet>
-          </>
+          <RNBottomSheet
+            backgroundStyle={{backgroundColor: 'white'}}
+            enablePanDownToClose
+            handleComponent={useHandle ? undefined : () => <Box />}
+            ref={ref}
+            onClose={onSheetClose}
+            snapPoints={snapsTo}>
+            {renderView && renderView()}
+          </RNBottomSheet>
         );
       }}
     />
