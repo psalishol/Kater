0c3e97a3d12ea853a1f4368bef49ead46bcdee12
psalishol <psalishol80@gmail.com>
2023-11-20 17:31:27 -0800

    add app constants

---
+++ src/constant/index.ts
@@ -0,0 +1,16 @@
+import {Dimensions} from 'react-native';
+import {Easing} from 'react-native-reanimated';
+
+// Dimensions -> for responsiveness
+export const screenWidth = Dimensions.get('window').width;
+export const screenHeight = Dimensions.get('window').height;
+
+// Easings -> For animation
+export const bezierEasing = Easing.bezier(0.65, 0, 0.35, 1);
+export const bezierCubicEaseOut = Easing.bezier(0.33, 1, 0.68, 1);
+export const linearEasing = Easing.linear;
+
+export const cubicEaseIn = Easing.bezier(0.32, 0, 0.67, 0);
+export const easeOutCirc = Easing.bezier(0, 0.55, 0.45, 1);
+export const easeOutSine = Easing.bezier(0.61, 1, 0.88, 1);
+export const easeOutQuard = Easing.bezier(0.5, 1, 0.89, 1);
