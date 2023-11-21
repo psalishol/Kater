297a98f05ffea57fe02ab10da76d838c0d4d1a68
psalishol <psalishol80@gmail.com>
2023-11-20 17:54:27 -0800

    add device lat lng atom

--- src/feature/home/state/index.ts
+++ src/feature/home/state/index.ts
@@ -1,3 +1,15 @@
 import {atom} from 'jotai';
 
+// DeviceLatLng defines the interface for the current user location
+// in latitude and longitude. this is used to fetch the actual location of the user
+// in human readable format. like 24 Ayetoro Ilupeju Lagos.
+export interface DeviceLatLng {
+  lat: number;
+  lng: number;
+}
+
+// Atom storing the state of the user current city.
 export const currentCityAtom = atom<string | undefined>(undefined);
+
+// Atom storing the state of device latitude and longitude.
+export const deviceLatLngAtom = atom<DeviceLatLng | undefined>(undefined);
