72d71df4887355a0337ee3fe2f2b2d28a47d87ac
psalishol <psalishol80@gmail.com>
2023-11-20 18:39:58 -0800

    remove latlng state

--- src/feature/home/state/index.ts
+++ src/feature/home/state/index.ts
@@ -1,21 +1,10 @@
 import {atom} from 'jotai';
 
-// DeviceLatLng defines the interface for the current user location
-// in latitude and longitude. this is used to fetch the actual location of the user
-// in human readable format. like 24 Ayetoro Ilupeju Lagos.
-export interface DeviceLatLng {
-  lat: number;
-  lng: number;
-}
-
 // TabMenu defines the type for the home tab menu
 export type TabMenu = 'Promos' | 'Products';
 
 // Atom storing the state of the user current city.
 export const currentCityAtom = atom<string | undefined>(undefined);
-
-// Atom storing the state of device latitude and longitude.
-export const deviceLatLngAtom = atom<DeviceLatLng | undefined>(undefined);
 
 // Atom storing the current selected menu tab.
 export const selectedMenuAtom = atom<TabMenu>('Promos');
