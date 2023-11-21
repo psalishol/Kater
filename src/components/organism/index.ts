45b5fd679e482fee01dce374bf003b128e84124b
psalishol <psalishol80@gmail.com>
2023-11-21 11:15:57 +0100

    export screen header

--- src/components/organism/index.ts
+++ src/components/organism/index.ts
@@ -3,3 +3,4 @@ export {default as GradientBackground} from './gradient-background';
 export {default as InlineBottomSheet} from './inline-bottom-sheet';
 export {default as Liftview} from './liftview';
 export {default as Overlay} from './overlay';
+export {default as ScreenHeader} from './screen-header';
