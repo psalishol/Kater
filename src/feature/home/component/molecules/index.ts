8eb6f8f43ae9e9bbc34b85c0666363a6ec11f196
psalishol <psalishol80@gmail.com>
2023-11-20 18:17:28 -0800

    export molecules

---
+++ src/feature/home/component/molecules/index.ts
@@ -0,0 +1,4 @@
+export {default as SearchBox} from './search-box';
+export {default as LocationChangerButton} from './location-changer-button';
+export {default as ViewPromoButton} from './view-promo-button';
+export {default as MessageStoreButton} from './message-store-button';
