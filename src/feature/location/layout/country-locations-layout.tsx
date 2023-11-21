785aaee1d5f7776e9583ece1adcd1974aa8867bf
psalishol <psalishol80@gmail.com>
2023-11-21 05:41:22 -0800

    style render item

--- src/feature/location/layout/country-locations-layout.tsx
+++ src/feature/location/layout/country-locations-layout.tsx
@@ -1,4 +1,10 @@
-import {Box, Entypo, ScrollView, Text} from '../../../components/atom';
+import {
+  Box,
+  Entypo,
+  Ionicons,
+  ScrollView,
+  Text,
+} from '../../../components/atom';
 import {InlineBottomSheet} from '../../../components/organism';
 import {useEffect, useState, useCallback} from 'react';
 import {
@@ -9,6 +15,7 @@ import {
 import {useAtom, useAtomValue} from 'jotai';
 import {getCountryCityState} from '../util';
 import {fonts} from '../../../themes/fonts';
+import {BottomSheetScrollView} from '@gorhom/bottom-sheet';
 
 const CountryLocation: React.FunctionComponent = () => {
   const [open, setOpen] = useAtom(openChangeLocation);
@@ -20,9 +27,10 @@ const CountryLocation: React.FunctionComponent = () => {
   return (
     <InlineBottomSheet
       show={open}
+      overlayOpacity={0.8}
       onOverlayPressed={handleClose}
       closeSheet={handleClose}
-      renderView={LocationDisplay}
+      renderView={() => <LocationDisplay />}
     />
   );
 };
@@ -45,25 +53,42 @@ const LocationDisplay = () => {
     }
   }, [currentLocation?.country, countryCityState]);
 
-  if (fetching) {
-    // Return activity indicator
-    return <></>;
-  }
-
   return (
-    <Box flex={1}>
-      <ScrollView>
+    <Box flex={1} bg="$white" borderTopEndRadius="sm" borderTopStartRadius="sm">
+      <Box
+        borderTopEndRadius="xs"
+        borderTopStartRadius="xs"
+        bg="$white"
+        height={40}
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="row">
+        <Entypo size={18} name="location-pin" color={'$black'} />
+        <Text
+          fontFamily={fonts.InterSemiBold}
+          fontSize={14}
+          ml="sm"
+          textAlign="center">
+          Switch location
+        </Text>
+      </Box>
+      <Box height={0.5} bg="$black" opacity={0.1} />
+
+      <BottomSheetScrollView style={{flex: 1}}>
         {countryCityState?.map((item, i) => {
           return (
-            <Box flexDirection="row" alignItems="center">
-              <Entypo size={18} name="location-pin" color={'$black'} />
-              <Text ml="xs" fontFamily={fonts.InterMedium} color="$black">
-                {item}
-              </Text>
+            <Box>
+              <Box height={45} px="md" flexDirection="row" alignItems="center">
+                <Ionicons name="location-outline" color={'$black'} />
+                <Text ml="sm" fontFamily={fonts.InterMedium} color="$black">
+                  {item.city}, {item.state}
+                </Text>
+              </Box>
+              <Box mt="xxs" height={0.5} bg="$black" opacity={0.1} />
             </Box>
           );
         })}
-      </ScrollView>
+      </BottomSheetScrollView>
     </Box>
   );
 };
