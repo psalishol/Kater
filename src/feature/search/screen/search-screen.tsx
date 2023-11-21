5101e3926d60c502737af0c7ad90156e2b7fa2bb
psalishol <psalishol80@gmail.com>
2023-11-21 13:38:14 +0100

    update: add search selectors

--- src/feature/search/screen/search-screen.tsx
+++ src/feature/search/screen/search-screen.tsx
@@ -2,20 +2,92 @@ import {useNavigation} from '@react-navigation/native';
 import {
   AntDesign,
   Box,
+  Feather,
   Ionicons,
+  MaterialCommunityIcons,
   Pressable,
+  Text,
   TextInput,
 } from '../../../components/atom';
 import {ScreenBackground} from '../../../components/organism';
 import {useCallback} from 'react';
 import {SearchScreenHeader} from '../component/organism';
+import {screenWidth} from '../../../constant';
+import {fonts} from '../../../themes/fonts';
+import {
+  SearchFilterSelector,
+  selectedSearchFilterAtom,
+  selectors,
+} from '../state';
+import {useAtomValue} from 'jotai';
 
 const SearchScreen: React.FunctionComponent = () => {
   return (
     <ScreenBackground color={'$white'}>
       <SearchScreenHeader />
+      <Box height={0.5} bg="$black" opacity={0.1} />
+      <SearchFilterSelectorComp />
     </ScreenBackground>
   );
 };
 
 export default SearchScreen;
+
+const SearchFilterSelectorComp = () => {
+  const selectedFilter = useAtomValue(selectedSearchFilterAtom);
+
+  const getIcon = (e: SearchFilterSelector, isSelected: boolean) => {
+    const color = isSelected ? '$white' : '$black';
+    if (e === 'Item') {
+      return <Feather size={14} name="package" color={color} />;
+    }
+    if (e === 'Service') {
+      return (
+        <MaterialCommunityIcons size={12} name="nature-people" color={color} />
+      );
+    }
+
+    if (e === 'Food') {
+      return (
+        <MaterialCommunityIcons
+          size={14}
+          name="room-service-outline"
+          color={color}
+        />
+      );
+    }
+  };
+
+  return (
+    <Box ml="sm" mt="sm" flexDirection="row" alignItems="center">
+      {selectors.map((e, i) => {
+        const selected = e === selectedFilter;
+        return (
+          <Box
+            borderRadius="xs"
+            justifyContent="center"
+            alignItems="center"
+            mr="sm"
+            style={{
+              backgroundColor: selected
+                ? 'rgba(0,210,0,1)'
+                : 'rgba(0,0,0,0.05)',
+            }}
+            height={25}
+            flexDirection="row"
+            width={screenWidth * 0.2}
+            key={i}>
+            {getIcon(e, selected)}
+            <Text
+              fontSize={14}
+              ml="xs"
+              color={selected ? '$white' : '$black'}
+              fontFamily={fonts.RobotoRegular}>
+              {e}
+            </Text>
+          </Box>
+        );
+      })}
+    </Box>
+  );
+};
