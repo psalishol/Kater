8a1251ba006fe002a39e2367f3570ea2734e29f7
psalishol <psalishol80@gmail.com>
2023-11-20 17:41:29 -0800

    add location changer button

--- src/feature/home/screen/home.tsx
+++ src/feature/home/screen/home.tsx
@@ -1,14 +1,44 @@
-import {Box, Text} from '../../../components/atom';
-import React from 'react';
+import {
+  Box,
+  Entypo,
+  ScrollView,
+  Text,
+  Touchable,
+} from '../../../components/atom';
+import React, {useCallback} from 'react';
+import {GradientBackground} from '../../../components/organism';
+import {fonts} from '../../../themes/fonts';
 
 const Home: React.FunctionComponent = () => {
   return (
-    <Box bg="$primary" flex={1} alignItems="center" justifyContent="center">
-      <Text mt="sm" color="$foreground">
-        Home screen
-      </Text>
-    </Box>
+    <GradientBackground>
+      <ScrollView>
+        <LocationChangerButton />
+      </ScrollView>
+    </GradientBackground>
   );
 };
 
 export default Home;
+
+const LocationChangerButton: React.FunctionComponent = () => {
+  const handlePress = useCallback(() => {}, []);
+
+  return (
+    <Touchable
+      px="xs"
+      mx="sm"
+      py="xxs"
+      borderRadius="xs"
+      style={{backgroundColor: 'rgba(0,0,0,0.1)'}}
+      alignSelf="baseline"
+      mt="md"
+      flexDirection="row"
+      alignItems="center">
+      <Entypo size={18} name="location-pin" color={'$black'} />
+      <Text ml="xs" fontFamily={fonts.InterMedium} color="$black">
+        Ikorodu, Lagos
+      </Text>
+    </Touchable>
+  );
+};
