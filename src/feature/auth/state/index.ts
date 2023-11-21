71c432a9859e629765d748925a47e07f07b9aa15
psalishol <psalishol80@gmail.com>
2023-11-21 15:17:27 +0100

    add auth state

---
+++ src/feature/auth/state/index.ts
@@ -0,0 +1,10 @@
+import {atom} from 'jotai';
+
+// Login credential
+export const loginEmailQueryAtom = atom<string>('');
+export const loginPasswordQueryAtom = atom<string>('');
+
+// signup credential
+export const signupEmailQueryAtom = atom<string>('');
+export const signupPasswordQueryAtom = atom<string>('');
+export const signupUsernameQueryAtom = atom<string>('');
