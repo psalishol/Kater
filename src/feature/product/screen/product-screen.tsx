cd11631365706a9b84c8edd9b259c557d67bc22e
psalishol <psalishol80@gmail.com>
2023-11-21 11:19:29 +0100

    add product screen

---
+++ src/feature/product/screen/product-screen.tsx
@@ -0,0 +1,12 @@
+import {Box} from '../../../components/atom';
+import {ScreenBackground} from '../../../components/organism';
+
+const ProductScreen: React.FunctionComponent = () => {
+  return (
+    <ScreenBackground>
+      <Box />
+    </ScreenBackground>
+  );
+};
+
+export default ProductScreen;
