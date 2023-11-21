883a1454c88f5ad4ab48c345eace52d951a02f16
psalishol <psalishol80@gmail.com>
2023-11-21 06:11:19 -0800

    implement location render based on filter query

--- src/feature/location/layout/country-locations-layout.tsx
+++ src/feature/location/layout/country-locations-layout.tsx
@@ -1,21 +1,18 @@
-import {
-  Box,
-  Entypo,
-  Ionicons,
-  ScrollView,
-  Text,
-} from '../../../components/atom';
+import {Box, Entypo, Ionicons, Text} from '../../../components/atom';
 import {InlineBottomSheet} from '../../../components/organism';
-import {useEffect, useState, useCallback} from 'react';
+import {useEffect, useState, useCallback, useMemo} from 'react';
 import {
   countryCityStateAtom,
   currentLocationAtom,
+  locationFilterQueryAtom,
   openChangeLocation,
 } from '../state';
 import {useAtom, useAtomValue} from 'jotai';
 import {getCountryCityState} from '../util';
 import {fonts} from '../../../themes/fonts';
 import {BottomSheetScrollView} from '@gorhom/bottom-sheet';
+import {LocationFilterInput} from '../component';
+import {ActivityIndicator} from 'react-native';
 
 const CountryLocation: React.FunctionComponent = () => {
   const [open, setOpen] = useAtom(openChangeLocation);
@@ -43,6 +40,8 @@ const LocationDisplay = () => {
   const [countryCityState, setCountryCityState] = useAtom(countryCityStateAtom);
   const [fetching, setFetching] = useState<boolean>(false);
 
+  const filterQuery = useAtomValue(locationFilterQueryAtom);
+
   useEffect(() => {
     if (currentLocation?.country && !countryCityState) {
       setFetching(true);
@@ -52,6 +51,31 @@ const LocationDisplay = () => {
       });
     }
   }, [currentLocation?.country, countryCityState]);
+
+  const data = useMemo(() => {
+    if (!filterQuery) {
+      return countryCityState;
+    } else {
+      if (!!countryCityState) {
+        return countryCityState.filter(e => {
+          if (
+            e.city
+              .toLowerCase()
+              .trim()
+              .includes(filterQuery.toLowerCase().trim()) ||
+            e.state
+              .toLowerCase()
+              .trim()
+              .includes(filterQuery.toLowerCase().trim())
+          ) {
+            return e;
+          }
+        });
+      }
+
+      return countryCityState;
+    }
+  }, [filterQuery]);
 
   return (
     <Box flex={1} bg="$white" borderTopEndRadius="sm" borderTopStartRadius="sm">
@@ -74,21 +98,35 @@ const LocationDisplay = () => {
       </Box>
       <Box height={0.5} bg="$black" opacity={0.1} />
 
-      <BottomSheetScrollView style={{flex: 1}}>
-        {countryCityState?.map((item, i) => {
-          return (
-            <Box>
-              <Box height={45} px="md" flexDirection="row" alignItems="center">
-                <Ionicons name="location-outline" color={'$black'} />
-                <Text ml="sm" fontFamily={fonts.InterMedium} color="$black">
-                  {item.city}, {item.state}
-                </Text>
+      <LocationFilterInput />
+
+      {fetching && (
+        <Box flex={1} justifyContent="center" alignItems="center">
+          <ActivityIndicator />
+        </Box>
+      )}
+
+      {!fetching && (
+        <BottomSheetScrollView style={{flex: 1}}>
+          {data?.map((item, i) => {
+            return (
+              <Box>
+                <Box
+                  height={45}
+                  px="md"
+                  flexDirection="row"
+                  alignItems="center">
+                  <Ionicons name="location-outline" color={'$black'} />
+                  <Text ml="sm" fontFamily={fonts.InterMedium} color="$black">
+                    {item.city}, {item.state}
+                  </Text>
+                </Box>
+                <Box mt="xxs" height={0.5} bg="$black" opacity={0.1} />
               </Box>
-              <Box mt="xxs" height={0.5} bg="$black" opacity={0.1} />
-            </Box>
-          );
-        })}
-      </BottomSheetScrollView>
+            );
+          })}
+        </BottomSheetScrollView>
+      )}
     </Box>
   );
 };
