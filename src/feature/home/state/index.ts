1e38926d1cb4210ee3e90c56e6077b37ce5738ff
psalishol <psalishol80@gmail.com>
2023-11-20 20:38:15 -0800

    rm device current location

--- src/feature/home/state/index.ts
+++ src/feature/home/state/index.ts
@@ -3,9 +3,6 @@ import {atom} from 'jotai';
 // TabMenu defines the type for the home tab menu
 export type TabMenu = 'Promos' | 'Products';
 
-// Atom storing the state of the user current city.
-export const currentCityAtom = atom<string | undefined>(undefined);
-
 // Atom storing the current selected menu tab.
 export const selectedMenuAtom = atom<TabMenu>('Promos');
 
