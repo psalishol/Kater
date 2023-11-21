0dcade760e2081270175aeed3f6317980ca90f6c
psalishol <psalishol80@gmail.com>
2023-11-21 14:56:24 +0100

    update: add tagline header

--- src/feature/auth/screens/login.tsx
+++ src/feature/auth/screens/login.tsx
@@ -1,15 +1,34 @@
 import {Box, Text} from '../../../components/atom';
 import React from 'react';
 import {useAppMode} from '../../light-dark-mode';
+import {GradientBackground} from '../../../components/organism';
+import {FlatButton} from '../../../components/molecules';
+import {fonts} from '../../../themes/fonts';
+import {screenHeight} from '../../../constant';
 
 const Login: React.FunctionComponent = () => {
   const {state, isLight} = useAppMode();
 
   console.log('state', state, isLight);
   return (
-    <Box bg="$primary" flex={1} alignItems="center" justifyContent="center">
-      <Text color="$foreground">Login screen</Text>
-    </Box>
+    <GradientBackground>
+      <Box style={{marginTop: screenHeight * 0.2}} />
+      <Text fontSize={16} mx="md" fontFamily={fonts.RobotoMedium}>
+        Welcome to your neighbourhood marketplace!{' '}
+      </Text>
+      <Text mx="md" mt="xs" fontSize={14} opacity={0.5}>
+        Kater lets you connect with your local businesses, getting what you need
+        in no time. Fresh, local, and convenient
+      </Text>
+      <FlatButton
+        mt="auto"
+        mb="lg"
+        color="$white"
+        label="Login"
+        bg={'$deepGreen'}
+        mx="md"
+      />
+    </GradientBackground>
   );
 };
 
