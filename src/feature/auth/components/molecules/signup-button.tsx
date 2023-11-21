74cb20461eadb790faabe20d60d847182b971e8a
psalishol <psalishol80@gmail.com>
2023-11-21 15:42:08 +0100

    update: implement button press logic

--- src/feature/auth/components/molecules/signup-button.tsx
+++ src/feature/auth/components/molecules/signup-button.tsx
@@ -1,9 +1,19 @@
+import {useNavigation} from '@react-navigation/native';
 import {AntDesign} from '../../../../components/atom';
 import {FlatButton} from '../../../../components/molecules';
+import {RootStackNavigationProp} from '../../../../navigation/types';
+import {useCallback} from 'react';
 
 const SignupButton: React.FunctionComponent = () => {
+  const navigation = useNavigation<RootStackNavigationProp<'SignUp'>>();
+
+  const handlePress = useCallback(() => {
+    navigation.navigate('SignUp');
+  }, [navigation]);
+
   return (
     <FlatButton
+      onPress={handlePress}
       mt="lg"
       color="$black"
       borderWidth={1}
