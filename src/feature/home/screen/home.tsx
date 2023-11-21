64002c5fcf9e651daea2792b979e3b858fb3b671
psalishol <psalishol80@gmail.com>
2023-11-21 11:17:37 +0100

    update: add home message button and followedstore new products

--- src/feature/home/screen/home.tsx
+++ src/feature/home/screen/home.tsx
@@ -1,20 +1,30 @@
-import {ScrollView} from '../../../components/atom';
+import {Box, ScrollView} from '../../../components/atom';
 import React from 'react';
 import {GradientBackground} from '../../../components/organism';
 import {LocationChangerButton, SearchBox} from '../component/molecules';
 import {Greeting} from '../component/atom';
-import {HomeTabMenu} from '../component/organism';
+import {
+  FollowedStoreNewProducts,
+  HomeTabMenu,
+  Promos,
+} from '../component/organism';
 import {CountryLocation, useGetDeviceLatLng} from '../../location';
+import {HomeHeaderMessageButton} from '../../message';
 
 const Home: React.FunctionComponent = () => {
   useGetDeviceLatLng();
   return (
     <GradientBackground>
       <ScrollView>
-        <LocationChangerButton />
+        <Box flexDirection="row" alignItems="center">
+          <LocationChangerButton />
+          <HomeHeaderMessageButton />
+        </Box>
         <Greeting />
         <SearchBox />
         <HomeTabMenu />
+        <Promos />
+        <FollowedStoreNewProducts />
       </ScrollView>
       <CountryLocation />
     </GradientBackground>
