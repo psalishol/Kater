85200a3fb89705c88a623819935996e37c097811
psalishol <psalishol80@gmail.com>
2023-11-22 09:54:49 +0100

    update sign in button

--- src/feature/auth/components/molecules/login-button.tsx
+++ src/feature/auth/components/molecules/login-button.tsx
@@ -1,8 +1,25 @@
+import {useAtom, useAtomValue} from 'jotai';
 import {FlatButton} from '../../../../components/molecules';
+import {loginEmailQueryAtom, loginPasswordQueryAtom} from '../../state';
 
 const LoginButton: React.FunctionComponent = () => {
+  const [email, setEmail] = useAtom(loginEmailQueryAtom);
+  const [password, setPassword] = useAtom(loginPasswordQueryAtom);
+
+  // handles user login
+  const handleSignIn = async () => {
+    try {
+      // Reset sign in form
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      console.log('error signing in', error);
+    }
+  };
+
   return (
     <FlatButton
+      onPress={handleSignIn}
       mt="2xl"
       color="$white"
       label="Login"
