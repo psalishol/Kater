14056a12a1a4fa7a3cb0f4ad0525c2e909a59ccb
psalishol <psalishol80@gmail.com>
2023-11-22 14:30:11 +0100

    add config

---
+++ config.ts
@@ -0,0 +1,6 @@
+export const authivate_token = 'e938934e420e785c7651c41e21c3ce746727f024';
+export const world_api_token =
+  '28775b08bemshf66c49b35181beep168539jsn21d0522b8bbc';
+export const google_api_key = 'AIzaSyAExqn-v-guxgZ8S1LyV7AttHLlmSBeqlU';
+
+export const project_id = 'kater';
