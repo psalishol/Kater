560bd00211d8cfc36eb56199f41afbb0bd798264
psalishol <psalishol80@gmail.com>
2023-11-22 02:57:38 +0100

    add user and account atom

---
+++ src/state/user.ts
@@ -0,0 +1,8 @@
+import {atom} from 'jotai';
+import {Account, User} from '../models';
+
+// Defines the user global state.
+export const userAtom = atom<User | undefined>(undefined);
+
+// defines the user account.
+export const accountsAtom = atom<Account[] | undefined>(undefined);
